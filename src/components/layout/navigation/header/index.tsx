1c43867d5914d65467d5a62e80d4f19fa462edb2
2xK <boroviksvladislavs@gmail.com>
2025-04-15 19:55:23 +0100

    style: add delay to nav link anims

--- src/components/layout/navigation/header/index.tsx
+++ src/components/layout/navigation/header/index.tsx
@@ -18,33 +18,39 @@ const Header = async () => {
 					<nav className="hidden gap-8 md:flex [&>a]:transition-all [&>a]:hover:font-semibold">
 						<Link
 							href={siteContent.links.stories.href}
-							className={"group relative flex items-center hover:pl-5"}
+							className={
+								"group relative flex items-center delay-100 hover:pl-5"
+							}
 						>
 							<TextIcon
 								className={
-									"absolute left-0 size-4 opacity-0 transition-opacity group-hover:opacity-100"
+									"absolute left-0 size-4 opacity-0 transition-opacity delay-100 group-hover:opacity-100"
 								}
 							/>
 							Browse
 						</Link>
 						<Link
 							href={siteContent.links.submit.href}
-							className={"group relative flex items-center hover:pl-5"}
+							className={
+								"group relative flex items-center delay-100 hover:pl-5"
+							}
 						>
 							<PencilLineIcon
 								className={
-									"absolute left-0 size-4 opacity-0 transition-opacity group-hover:opacity-100"
+									"absolute left-0 size-4 opacity-0 transition-opacity delay-100 group-hover:opacity-100"
 								}
 							/>
 							Submit
 						</Link>
 						<Link
 							href={siteContent.links.claimStory.href}
-							className={"group relative flex items-center hover:pl-5"}
+							className={
+								"group relative flex items-center delay-100 hover:pl-5"
+							}
 						>
 							<FilePenLine
 								className={
-									"absolute left-0 size-4 opacity-0 transition-opacity group-hover:opacity-100"
+									"absolute left-0 size-4 opacity-0 transition-opacity delay-100 group-hover:opacity-100"
 								}
 							/>
 							Claim
@@ -58,11 +64,13 @@ const Header = async () => {
 										)
 									: siteContent.links.login.href
 							}
-							className={"group relative flex items-center hover:pl-5"}
+							className={
+								"group relative flex items-center delay-100 hover:pl-5"
+							}
 						>
 							<CircleUserIcon
 								className={
-									"absolute left-0 size-4 opacity-0 transition-opacity group-hover:opacity-100"
+									"absolute left-0 size-4 opacity-0 transition-opacity delay-100 group-hover:opacity-100"
 								}
 							/>
 							Profile
